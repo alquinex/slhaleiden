aa8522cf22febff74ad0b765471a56ce4e4f9ffe
Alusine Q. Sesay <alusineq.sesay@Alusines-MacBook-Pro.local>
2020-11-02 12:26:32 +0100

    gatsby-contentful-connection-setup

---
+++ slha-leiden/gatsby-config.js
@@ -0,0 +1,23 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
+}
